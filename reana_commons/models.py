984422e3f75997babd66e4b8e2b5ca2b50686a98
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-05-31 17:48:01 +0200

    models: Job table
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

--- reana_commons/models.py
+++ reana_commons/models.py
@@ -28,7 +28,7 @@ import enum
 import uuid
 
 from sqlalchemy import (Column, Enum, ForeignKey, Integer, String,
-                        UniqueConstraint)
+                        UniqueConstraint, Boolean)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import JSONType, UUIDType
@@ -174,3 +174,43 @@ class Workflow(Base, Timestamp):
             #     'An error occurred while updating workflow: {0}'.
             #     format(str(e)))
             raise e
+
+
+class Job(Base, Timestamp):
+    """Job table."""
+
+    __tablename__ = 'job'
+
+    id_ = Column(UUIDType, primary_key=True)
+    workflow_uuid = Column(UUIDType, primary_key=True)
+    status = Column(String(30))
+    job_type = Column(String(30))
+    cvmfs_mounts = Column(String(256))
+    shared_file_system = Column(Boolean)
+    docker_img = Column(String(256))
+    experiment = Column(String(256))
+    cmd = Column(String(1024))
+    env_vars = Column(String(1024))
+    restart_count = Column(Integer)
+    max_restart_count = Column(Integer)
+    deleted = Column(Boolean)
+    logs = Column(String)
+
+    def __init__(self, id_, workflow_uuid, status, job_type, cvmfs_mounts,
+                 shared_file_system, docker_img, experiment, cmd, env_vars,
+                 restart_count, max_restart_count, deleted, logs=None):
+        """Initialize job table."""
+        self.id_ = id_
+        self.workflow_uuid = workflow_uuid
+        self.status = status
+        self.job_type = job_type
+        self.cvmfs_mounts = cvmfs_mounts
+        self.shared_file_system = shared_file_system
+        self.docker_img = docker_img
+        self.experiment = experiment
+        self.cmd = cmd
+        self.env_vars = env_vars
+        self.restart_count = restart_count
+        self.max_restart_count = max_restart_count
+        self.deleted = deleted
+        self.logs = logs
