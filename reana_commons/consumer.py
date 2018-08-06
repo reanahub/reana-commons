e6e613a441468cf59af4c71ab3375c57809b4d03
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-08-06 09:36:40 +0200

    global: merge reana-workflow-commons
    * ADD consumer.py publisher.py and config.py from
      reana-workflow-commons, to merge the two repositories.
    
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

---
+++ reana_commons/consumer.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# REANA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# REANA; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307, USA.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+
+"""REANA MQ Consumer."""
+
+import pika
+
+from .config import BROKER_PASS, BROKER_PORT, BROKER_URL, BROKER_USER
+
+
+class Consumer:
+    """Base MQ Consumer."""
+
+    def __init__(self, queue):
+        """Constructor."""
+        self.broker_credentials = pika.PlainCredentials(BROKER_USER,
+                                                        BROKER_PASS)
+        self._params = pika.connection.ConnectionParameters(
+            BROKER_URL, BROKER_PORT, '/', self.broker_credentials)
+        self._conn = None
+        self._channel = None
+        self._queue = queue
+
+    def connect(self):
+        """Connect to MQ channel."""
+        if not self._conn or self._conn.is_closed:
+            self._conn = pika.BlockingConnection(self._params)
+            self._channel = self._conn.channel()
+            self._channel.basic_qos(prefetch_count=1)
+            self._channel.queue_declare(queue=self._queue)
+
+    def on_message(self, channel, method_frame, header_frame, body):
+        """On new message event handler."""
+        # this method should be overriden by the inheriting class
+        pass
+
+    def consume(self):
+        """Start consuming incoming messages."""
+        while True:
+            self.connect()
+            self._channel.basic_consume(self.on_message, self._queue)
+            try:
+                self._channel.start_consuming()
+            except KeyboardInterrupt:
+                self._channel.stop_consuming()
+                self._conn.close()
+            except pika.exceptions.ConnectionClosedByBroker:
+                # Uncomment this to make the example not attempt recovery
+                # from server-initiated connection closure, including
+                # when the node is stopped cleanly
+                # except pika.exceptions.ConnectionClosedByBroker:
+                #     pass
+                continue
---
+++ reana_commons/publisher.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# REANA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# REANA; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307, USA.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+
+import json
+import logging
+
+import pika
+
+from .config import (BROKER_PASS, BROKER_PORT, BROKER_URL, BROKER_USER,
+                     EXCHANGE, ROUTING_KEY, STATUS_QUEUE)
+
+
+class Publisher:
+    """Progress publisher to MQ."""
+
+    def __init__(self):
+        self.broker_credentials = pika.PlainCredentials(BROKER_USER,
+                                                        BROKER_PASS)
+        self._params = pika.connection.ConnectionParameters(
+            BROKER_URL, BROKER_PORT, '/', self.broker_credentials)
+        self._conn = None
+        self._channel = None
+
+    def connect(self):
+        """Connect to MQ channel."""
+        if not self._conn or self._conn.is_closed:
+            self._conn = pika.BlockingConnection(self._params)
+            self._channel = self._conn.channel()
+            self._channel.queue_declare(queue=STATUS_QUEUE)
+
+    def _publish(self, msg):
+        self._channel.basic_publish(exchange=EXCHANGE,
+                                    routing_key=ROUTING_KEY,
+                                    body=json.dumps(msg))
+        logging.debug('Publisher: message sent: %s', msg)
+
+    def publish_workflow_status(self, workflow_uuid, status,
+                                logs='', message=None):
+        """Publish msg, reconnecting if necessary."""
+        try:
+            msg = {"workflow_uuid": workflow_uuid,
+                   "logs": logs,
+                   "status": status,
+                   "message": message}
+            self._publish(msg)
+        except pika.exceptions.ConnectionClosed:
+            logging.debug('Publisher: ConnectionClosed, reconnecting.')
+            print('Publisher: ConnectionClosed, reconnecting.')
+            self.connect()
+            self._publish(msg)
+
+    def close(self):
+        if self._conn and self._conn.is_open:
+            logging.debug('Publisher: closing queue connection')
+            self._conn.close()
