5cc55379ee71dff73ae8ade986975532cf5acdaf
Diego Rodriguez <diego.rodriguez@cern.ch>
2018-12-13 13:48:15 +0100

    installation: kubernetes as extra

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ extras_require = {
         'sphinx-rtd-theme>=0.1.9',
     ],
     'tests': tests_require,
+    'kubernetes': [
+        'kubernetes>=6.0.0',
+    ],
 }
 
 extras_require['all'] = []
@@ -56,7 +59,6 @@ install_requires = [
     'click>=7.0,<8.0',
     'jsonschema[format]>=2.6.0,<2.7',
     'kombu>=4.2.0,<5.0',
-    'kubernetes>=6.0.0',
     'Werkzeug==0.14.1',
 ]
 
