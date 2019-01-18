27c6b0e3101d5bbbf805e670b997b68d2dde2e59
Diego Rodriguez <diego.rodriguez@cern.ch>
2019-01-18 14:39:20 +0100

    tasks: remove stop workflow task
    * Stop workflow is fully implemented inside RWC now
      (closes reanahub/reana-workflow-controller#157).

--- reana_commons/tasks.py
+++ reana_commons/tasks.py
@@ -11,7 +11,6 @@ import importlib
 import logging
 
 from celery import shared_task
-from celery.task.control import revoke
 from kubernetes.client.rest import ApiException
 
 from reana_commons.api_client import JobControllerAPIClient
@@ -20,33 +19,6 @@ from reana_commons.k8s.api_client import (current_k8s_batchv1_api_client,
                                           current_k8s_corev1_api_client)
 
 log = logging.getLogger(__name__)
-
-
-@shared_task(name='tasks.stop_workflow',
-             ignore_result=True)
-def stop_workflow(workflow_uuid, job_list):
-    """Stop a workflow.
-
-    :param workflow_uuid: UUID of the workflow to be stopped.
-    :param job_list: List of job identifiers which where created by the given
-        workflow.
-    """
-    rjc_api_client = JobControllerAPIClient('reana-job-controller')
-    try:
-        log.info('Stopping workflow {} Celery task ...'.format(workflow_uuid))
-        revoke(workflow_uuid, terminate=True)
-        for job_id in job_list:
-            log.info('Stopping job {} from workflow {} ...'.format(
-                job_id, workflow_uuid))
-            response, http_response = rjc_api_client._client.jobs.delete_job(
-                job_id=job_id).result()
-            log.info(response)
-            log.info(http_response)
-    except Exception as e:
-        log.error('Something went wrong while stopping workflow {} ...'.format(
-            workflow_uuid
-        ))
-        log.error(e)
 
 
 def reana_ready():
