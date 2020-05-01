9fe5399b6555d7f49c0a6793434b05bdecacf946
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-05-01 16:27:34 +0200

    mail: use authentication only if production
    * Allows testing email notifications workflow locally
      (addresses reanahub/reana#308).

--- reana_commons/email.py
+++ reana_commons/email.py
@@ -24,14 +24,14 @@ def send_email(receiver_email, subject, body, login_email=REANA_EMAIL_LOGIN,
                sender_email=REANA_EMAIL_SENDER):
     """Send emails from REANA platform."""
     message = 'Subject: {subject}\n\n{body}'.format(subject=subject, body=body)
-    if os.getenv('FLASK_ENV') == 'development':
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(REANA_EMAIL_SMTP_SERVER,
+                      REANA_EMAIL_SMTP_PORT) as server:
+        if os.getenv('FLASK_ENV') != 'development':
+            server.starttls(context=context)
+            server.login(login_email, REANA_EMAIL_PASSWORD)
+        server.sendmail(sender_email, receiver_email, message)
         logging.info('Email sent, login: {}, sender: {}, receiver: {}'
                      .format(login_email, sender_email, receiver_email))
         logging.info('Body:\n{}'.format(message))
-    else:
-        context = ssl.create_default_context()
-        with smtplib.SMTP(REANA_EMAIL_SMTP_SERVER,
-                          REANA_EMAIL_SMTP_PORT) as server:
-            server.starttls(context=context)
-            server.login(login_email, REANA_EMAIL_PASSWORD)
-            server.sendmail(sender_email, receiver_email, message)
