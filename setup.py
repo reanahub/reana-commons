09fec740c753b832ea5f4f3819f191d26997508f
Alputer <alp.tuna.453@gmail.com>
2024-11-05 15:30:02 +0100

    feat(snakemake): upgrade to Snakemake 8.27.1 (#476)
    Remove redundant dependencies already included in the Snakemake
    package and update the Snakemake version. Relying on Snakemake
    to manage these dependencies is more efficient and less error-prone
    than defining them manually in REANA.

--- setup.py
+++ setup.py
@@ -40,11 +40,7 @@ extras_require = {
     "snakemake": [
         "snakemake==7.32.4 ; python_version<'3.11'",
         "pulp>=2.7.0,<2.8.0 ; python_version<'3.11'",
-        "snakemake==8.24.1 ; python_version>='3.11'",
-        "snakemake-interface-common==1.17.4 ; python_version>='3.11'",
-        "snakemake-interface-executor-plugins==9.3.2 ; python_version>='3.11'",
-        "snakemake-interface-storage-plugins==3.3.0 ; python_version>='3.11'",
-        "snakemake-interface-report-plugins==1.1.0 ; python_version>='3.11'",
+        "snakemake==8.27.1 ; python_version>='3.11'",
     ],
     "snakemake-xrootd": [
         "snakemake-storage-plugin-xrootd==0.1.4 ; python_version>='3.11'",
