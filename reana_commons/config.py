25a2479757c2a2083f9cca89f360f137d3f272f2
Alexander Sohn <alexander.sohn@cern.ch>
2026-08-19 11:17:03 +0200

    feat(auth): job-controller client on loopback, spec sync (#545)
    - Sync bundled reana_server.json with the webhook test-delivery param
    - Job-controller client on loopback, resync bundled reana-server spec
    - Sync bundled reana-server spec (500 on gitlab endpoints)

--- reana_commons/config.py
+++ reana_commons/config.py
@@ -244,7 +244,7 @@ OPENAPI_SPECS = {
         "reana_server.json",
     ),
     "reana-job-controller": (
-        "http://{address}:{port}".format(address="0.0.0.0", port="5000"),
+        "http://{address}:{port}".format(address="127.0.0.1", port="5000"),
         "reana_job_controller.json",
     ),
 }
